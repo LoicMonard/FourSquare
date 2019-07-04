764c40fe837c7f19f71705908809f64f90b229df
LoicMonard <monard@intechinfo.fr>
2019-07-04 16:34:43 +0200

    Retrieve user location
    Signed-off-by: LoicMonard <monard@intechinfo.fr>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,113 @@
-package com.example.myapplication
+package com.example.foursquare
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var locationManager: LocationManager
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps : Location? = null
+    private var locationNetwork : Location? = null
+
+    private var permissions = arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.INTERNET)
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.activity_main)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, 10)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getLocation()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        if(hasGps || hasNetwork) {
+            if(hasGps) {
+                Log.d("CodeAndroidLocation", "hasGps")
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if(location!=null) {
+                            locationGps = location
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                })
+                val localGpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if(localGpsLocation!=null) {
+                    locationGps = localGpsLocation
+                }
+            }
+            if(hasNetwork) {
+                Log.d("CodeAndroidLocation", "hasNetwork")
+                locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 0F, object : LocationListener {
+                    override fun onLocationChanged(location: Location?) {
+                        if(location!=null) {
+                            locationNetwork = location
+                        }
+                    }
+
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderEnabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                    override fun onProviderDisabled(provider: String?) {
+                        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                    }
+
+                })
+                val localNetworkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+                if(localNetworkLocation!=null) {
+                    locationNetwork = localNetworkLocation
+                }
+            }
+
+            Toast.makeText(this, "Hi there! This is a Toast.", Toast.LENGTH_LONG).show()
+
+            if(locationGps!=null && locationNetwork!=null) {
+                if(locationGps!!.accuracy > locationNetwork!!.accuracy) {
+                    Log.d("CodeAndroidLocation", "Network latitude: " + locationNetwork!!.latitude)
+                    Log.d("CodeAndroidLocation", "Network longitude: " + locationNetwork!!.longitude)
+                } else {
+                    Log.d("CodeAndroidLocation", "GPS latitude: " + locationGps!!.latitude)
+                    Log.d("CodeAndroidLocation", "GPS longitude: " + locationGps!!.longitude)
+                }
+            }
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
     }
 }
